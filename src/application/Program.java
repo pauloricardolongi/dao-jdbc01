4696e7a2df882cd024160a0f8f8f5cfbf8627f16
pauloricardolongi <pauloricardolong@gmail.com>
2021-07-06 10:23:52 -0300

    update implementacao

--- src/application/Program.java
+++ src/application/Program.java
@@ -39,6 +39,11 @@ public class Program {
         sellerDao.insert(newSeller);
         System.out.println("Insert! New id = " + newSeller.getId());
         
+        System.out.println("\n===TEST 5: seller update ===");
+        seller = sellerDao.findById(1);
+        seller.setName("Paulo Santo");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
 	}
 
 }
